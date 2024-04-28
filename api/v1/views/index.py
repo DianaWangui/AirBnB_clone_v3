0b6f9b3f2f3d0468aad38e9f103f3bc60f6e81af
DianaWangui <dianawanguikt048@gmail.com>
2024-04-28 20:57:58 +0300

    update index with an endpoint that retrieves the number of each object type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,26 @@
 """Index module for the API"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Returns status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Returns stats"""
+ 
+    stats_count = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+
+    return jsonify(stats_count)
