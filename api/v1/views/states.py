023e6b57fde39ea741a215685bf8a62eceddfa48
DianaWangui <dianawanguikt048@gmail.com>
2024-05-03 08:15:02 +0300

    change state route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -26,7 +26,7 @@ def get_state_id(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a states of the given id"""
     state = storage.get(State, state_id)
