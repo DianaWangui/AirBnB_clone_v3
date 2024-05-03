91135fdf7eb0b1a742791796168e6e04d2fedf80
DianaWangui <dianawanguikt048@gmail.com>
2024-05-03 08:12:58 +0300

    change state route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,6 +5,7 @@ from models.state import State
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Returns all states"""
@@ -25,7 +26,7 @@ def get_state_id(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """Deletes a states of the given id"""
     state = storage.get(State, state_id)
